2414de5a41991dad4ff60294ea68ffa557b108ab
lilou <lilou@coocaa.com>
2021-04-28 17:30:55 +0800

    rename to lyloou

--- spring-monitor-demo/src/main/java/com/lyloou/demo/ScheduleService.java
+++ spring-monitor-demo/src/main/java/com/lyloou/demo/ScheduleService.java
@@ -7,6 +7,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
 /**
  * @author lilou
  * @since 2021/3/4
@@ -22,5 +28,18 @@ public class ScheduleService {
     @Scheduled(cron = "0/10 * * * * *")
     public void runPer10s() {
         log.info("runPer10s...");
+    }
+
+    public static void main(String[] args) {
+        List<BigDecimal> list = Arrays.asList(BigDecimal.valueOf(1.0), BigDecimal.valueOf(2.0));
+
+        final BigDecimal bigDecimal = BigDecimal.valueOf(1.0);
+        AtomicReference<BigDecimal> sum = new AtomicReference<>(bigDecimal);
+        final List<BigDecimal> decimalList = list.stream().map(v -> {
+            sum.set(sum.get().add(v));
+            return v;
+        }).collect(Collectors.toList());
+        System.out.println(sum.get());
+        System.out.println(decimalList);
     }
 }
